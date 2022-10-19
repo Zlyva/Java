0668657797dd58a334dd4fc277edd30ba31870c9
Artem <115576803+Zlyva@users.noreply.github.com>
2022-10-19 12:47:17 +0300

    Part1

---
+++ Part1/ArraySort.java
@@ -0,0 +1,27 @@
+package Prometeus.Part1;
+
+public class ArraySort {
+
+	public static void main(String[] args) {
+		int[] array = {30, 2, 10, 4, 6};
+        int length = array.length;
+		
+        boolean isSorted = false;
+        int buf;
+        while(!isSorted) {
+            isSorted = true;
+		for (int i = 0; i < length-1; i++) {
+            if(array[i]>array[i+1]){
+                isSorted = false;
+                buf = array[i];
+                array[i] = array[i+1];
+                array[i+1] = buf;
+            }
+            
+        }
+
+        } 
+        for (int i = 0; i < length; i++) 
+            System.out.print(array[i] + " ");
+    }        
+}
